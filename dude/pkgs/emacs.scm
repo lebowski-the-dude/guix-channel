40d4005a2e8a36bdf740a0d985c17e9e38554f39
Dude <>
2023-01-04 23:16:12 +0300

    update stuff package

--- dude/pkgs/emacs.scm
+++ dude/pkgs/emacs.scm
@@ -6,8 +6,8 @@
   #:use-module (gnu packages emacs-xyz))
 
 (define-public emacs-stuff
-  (let ((revision "2")
-        (commit "9f0acca7414548c2398d11e61c9f1ecbeed5dfe4"))
+  (let ((revision "3")
+        (commit "7c72fbb4cf56b96b697c0fb8d1706e4ecc419cc6"))
     (package
       (name "emacs-stuff")
       (version (git-version "0.1" revision commit))
@@ -19,7 +19,7 @@
 	       (commit commit)))
 	 (file-name (git-file-name name version))
 	 (sha256
-	  (base32 "06cms02hm2k59z9kigl51a6n1aljdzr9nsishfavq6yc6f0y3fls"))))
+	  (base32 "18x5sh568pch54ryb8q289y4hya6bd5jnwysrnss18ybfar4rgg8"))))
       (build-system emacs-build-system)
       (propagated-inputs
        (list emacs-async))
