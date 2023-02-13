056f7494b32a29e8ac789d4dab1ff27f354b4d25
Dude <>
2023-02-14 01:00:46 +0300

    update stuff package and add home config

--- dude/pkgs/emacs.scm
+++ dude/pkgs/emacs.scm
@@ -6,8 +6,8 @@
   #:use-module (gnu packages emacs-xyz))
 
 (define-public emacs-stuff
-  (let ((revision "3")
-        (commit "7c72fbb4cf56b96b697c0fb8d1706e4ecc419cc6"))
+  (let ((revision "4")
+        (commit "2efd9c7dbd685740007fa9fda8fee09ec60550fe"))
     (package
       (name "emacs-stuff")
       (version (git-version "0.1" revision commit))
@@ -19,7 +19,7 @@
 	       (commit commit)))
 	 (file-name (git-file-name name version))
 	 (sha256
-	  (base32 "18x5sh568pch54ryb8q289y4hya6bd5jnwysrnss18ybfar4rgg8"))))
+	  (base32 "1mayy270dkqsimmm7b5p85jbj9p53xyr42g19iz7ilym3gyigfqn"))))
       (build-system emacs-build-system)
       (propagated-inputs
        (list emacs-async))
