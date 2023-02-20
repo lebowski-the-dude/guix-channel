c4b9541ef2d12657988138a93623461a59542b32
Dude <>
2023-02-20 22:52:29 +0300

    wip

--- dude/pkgs/python.scm
+++ dude/pkgs/python.scm
@@ -315,11 +315,12 @@ Includes the libtcodpy module for backwards compatibility with older projects.")
        '(#:tests? #f
          #:phases (modify-phases %standard-phases
                     (delete 'sanity-check)
-                    ;; (add-after 'unpack 'patch
-                    ;;   (lambda* (#:key inputs #:allow-other-keys)
-                    ;;     (let ((xrandr (assoc-ref inputs "xrandr"))
-                    ;;           (gawk (assoc-ref inputs "gawk"))
-                    ;;           (grep (assoc-ref inputs "grep")))
+                    (add-after 'unpack 'patch
+                      (lambda* (#:key inputs #:allow-other-keys)
+                        (let ((xrandr (assoc-ref inputs "xrandr"))
+                              (gawk (assoc-ref inputs "gawk"))
+                              (grep (assoc-ref inputs "grep")))
+                          )))
                     ;;       (substitute* '("daemon/daemon.py")
                     ;;         (("xrandr")
                     ;;          (string-append xrandr "/bin/xrandr"))
