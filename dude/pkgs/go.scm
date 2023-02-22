ee434b617926e30b8dbe76e611793177c0fd507c
Dude <>
2023-02-22 17:53:03 +0300

    wip

--- dude/pkgs/go.scm
+++ dude/pkgs/go.scm
@@ -5,27 +5,46 @@
   #:use-module (guix build-system go)
   #:use-module (gnu packages golang))
 
-(define-public go-github-com-lebowski-the-dude-daemon
+(define-public go-github-com-lebowski-the-dude-test-daemon
   (let ((revision "1")
-        (commit "3e8f7c5f4fdef6f1545db8479c5a6d357555a579"))
+        (commit "524d8564b7a1bc83cb419633949ae24860acb49a"))
     (package
-      (name "go-github-com-lebowski-the-dude-daemon")
+      (name "go-github-com-lebowski-the-dude-test-daemon")
       (version (git-version "0.1" revision commit))
       (source
        (origin
          (method git-fetch)
          (uri
           (git-reference
-           (url "https://github.com/lebowski-the-dude/daemon.git")
+           (url "https://github.com/lebowski-the-dude/test-daemon")
            (commit commit)))
          (file-name (git-file-name name version))
          (sha256
           (base32 "1fc5qk3xnbiywp2j3kpb2zp3rrndvj1mrdiij4nkidyym9l2i53y"))))
       (build-system go-build-system)
-      (arguments
-       `(#:import-path "github.com/lebowski-the-dude/daemon"))
       (native-inputs
        (list go-github-com-sevlyar-go-daemon))
+      (propagated-inputs
+       (list xrandr
+             gawk
+             grep))
+      (arguments
+       `(#:import-path "github.com/lebowski-the-dude/test-daemon"
+         #:phases (modify-phases %standard-phases
+                    (delete 'sanity-check)
+                    (add-after 'unpack 'patch
+                      (lambda* (#:key inputs #:allow-other-keys)
+                        (let ((xrandr (assoc-ref inputs "xrandr"))
+                              (gawk (assoc-ref inputs "gawk"))
+                              (grep (assoc-ref inputs "grep")))
+                          (substitute* '("daemon/daemon.py")
+                            (("xrandr")
+                             (string-append xrandr "/bin/xrandr"))
+                            )))))))
+                            ;; (("awk")
+                            ;;  (string-append gawk "/bin/awk"))
+                            ;; (("grep")
+                            ;;  (string-append grep "/bin/grep")))))))))
       (home-page "")
       (synopsis "")
       (description "")
