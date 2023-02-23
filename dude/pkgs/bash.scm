a1ed169e74f1e32b7c8fb9d92458446c4c3658ba
Dude <>
2023-02-23 17:11:37 +0300

    wip

---
+++ dude/pkgs/bash.scm
@@ -0,0 +1,58 @@
+(define-module (dude pkgs bash)
+  #:use-module ((guix licenses) #:prefix license:)
+  #:use-module (guix packages)
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages base)
+  #:use-module (gnu packages gawk)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system trivial))
+
+(define-public monitor-checker
+  (let ((revision "0")
+        (commit "c7b480206e7e30648fd56f0571c8e66335391be7"))
+    (package
+      (name "monitor-checker")
+      (version (git-version "0.0.0" revision commit))
+      (source
+       (origin
+	 (method git-fetch)
+	 (uri (git-reference
+	       (url "https://github.com/lebowski-the-dude/utils")
+	       (commit commit)))
+	 (file-name (git-file-name name version))
+	 (sha256
+	  (base32 "1mayy270dkqsimmm7b5p85jbj9p53xyr42g19iz7ilym3gyigfqn"))))
+      (build-system trivial-build-system)
+      (propagated-inputs
+       (list xrandr
+             gawk
+             grep))
+      (arguments
+       `(#:import-path "github.com/lebowski-the-dude/test-daemon"
+         #:phases (modify-phases %standard-phases
+                    (delete 'sanity-check)
+                    (add-after 'unpack 'patch
+                      (lambda* (#:key inputs #:allow-other-keys)
+                        (let ((xrandr (assoc-ref inputs "xrandr"))
+                              (gawk (assoc-ref inputs "gawk"))
+                              (grep (assoc-ref inputs "grep")))
+                          (substitute* "MonitorChecker.sh"
+                            (("xrandr")
+                             (string-append xrandr "/bin/xrandr"))
+                            (("awk")
+                             (string-append gawk "/bin/awk"))
+                            (("grep")
+                             (string-append grep "/bin/grep")))))))
+         #:builder
+         (begin
+           (use-modules (guix build utils))
+           ;; copy source
+           (copy-file (string-append (assoc-ref %build-inputs "source")
+                                     "/MonitorChecker.sh")
+                      ".")
+           ;; install phase
+           (install-file "MonitorChecker.sh" (string-append %output "/bin")))))
+      (home-page "https://github.com/lebowski-the-dude/utils")
+      (synopsis "")
+      (description "")
+      (license license:gpl3+))))
